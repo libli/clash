60d4cf1cd8b4145a3cda6ae7cedb54acdf9dc975
libli <libli@tencent.com>
2023-10-06 15:16:52 +0800

    add module

--- Script/bwg-info.js
+++ Script/bwg-info.js
@@ -6,29 +6,26 @@ $httpClient.get(requestUrl, function(error, response, data){
     let jsonData = JSON.parse(data);
 
     // 获取所需的数据
-    let os = jsonData.os;
-    let nodeDatacenter = jsonData.node_datacenter;
-    let plan = jsonData.plan;
-    let planRam = jsonData.plan_ram;
-    let planDisk = jsonData.plan_disk;
     let ipAddresses = jsonData.ip_addresses.join(', ');
+    let nodeDatacenter = jsonData.node_datacenter;
+    let os = jsonData.os;
+    let plan = jsonData.plan;
+    let planRam = bytesToSize(jsonData.plan_ram);
+    let planDisk = bytesToSize(jsonData.plan_disk);
     let dataCounter = jsonData.data_counter * jsonData.monthly_data_multiplier;
     let dataNextReset = new Date(jsonData.data_next_reset * 1000);
     let planMonthlyData = jsonData.plan_monthly_data * jsonData.monthly_data_multiplier;
 
-    // 计算最长的标签长度
-    let maxLabelLength = Math.max("OS".length, "数据中心".length, "Plan".length, "RAM".length, "Disk".length, "IP地址".length);
-
     // 格式化内容
     let content = [
-        `OS:${' '.repeat(maxLabelLength - "OS".length)} ${os}`,
-        `数据中心:${' '.repeat(maxLabelLength - "数据中心".length)} ${nodeDatacenter}`,
-        `Plan:${' '.repeat(maxLabelLength - "Plan".length)} ${plan}`,
-        `RAM:${' '.repeat(maxLabelLength - "RAM".length)} ${planRam}`,
-        `Disk:${' '.repeat(maxLabelLength - "Disk".length)} ${planDisk}`,
-        `IP地址:${' '.repeat(maxLabelLength - "IP地址".length)} ${ipAddresses}`,
-        `用量：${bytesToSize(dataCounter)}|${bytesToSize(planMonthlyData)}`,
-        `重置：${dataNextReset.getFullYear()}年${dataNextReset.getMonth() + 1}月${dataNextReset.getDate()}日`
+        `IP: ${ipAddresses}`,
+        `用量：${bytesToSize(dataCounter)} | ${bytesToSize(planMonthlyData)}`,
+        `重置：${dataNextReset.getFullYear()}年${dataNextReset.getMonth() + 1}月${dataNextReset.getDate()}日`,
+        `Plan: ${plan}`,
+        `IDC: ${nodeDatacenter}`,
+        `OS: ${os}`,
+        `RAM: ${planRam}`,
+        `Disk: ${planDisk}`
     ];
 
     let now = new Date();
