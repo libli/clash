4c6baaa972368a51ca0be865b28b6148f5a9f43b
libli <libli@tencent.com>
2023-10-06 15:10:57 +0800

    add module

--- Script/bwg-info.js
+++ Script/bwg-info.js
@@ -5,8 +5,31 @@ $httpClient.get(requestUrl, function(error, response, data){
     // 解析 JSON 数据
     let jsonData = JSON.parse(data);
 
-    let nodeIp = jsonData.node_ip;
+    // 获取所需的数据
     let os = jsonData.os;
+    let nodeDatacenter = jsonData.node_datacenter;
+    let plan = jsonData.plan;
+    let planRam = jsonData.plan_ram;
+    let planDisk = jsonData.plan_disk;
+    let ipAddresses = jsonData.ip_addresses.join(', ');
+    let dataCounter = jsonData.data_counter * jsonData.monthly_data_multiplier;
+    let dataNextReset = new Date(jsonData.data_next_reset * 1000);
+    let planMonthlyData = jsonData.plan_monthly_data * jsonData.monthly_data_multiplier;
+
+    // 计算最长的标签长度
+    let maxLabelLength = Math.max("OS".length, "数据中心".length, "Plan".length, "RAM".length, "Disk".length, "IP地址".length);
+
+    // 格式化内容
+    let content = [
+        `OS:${' '.repeat(maxLabelLength - "OS".length)} ${os}`,
+        `数据中心:${' '.repeat(maxLabelLength - "数据中心".length)} ${nodeDatacenter}`,
+        `Plan:${' '.repeat(maxLabelLength - "Plan".length)} ${plan}`,
+        `RAM:${' '.repeat(maxLabelLength - "RAM".length)} ${planRam}`,
+        `Disk:${' '.repeat(maxLabelLength - "Disk".length)} ${planDisk}`,
+        `IP地址:${' '.repeat(maxLabelLength - "IP地址".length)} ${ipAddresses}`,
+        `用量：${bytesToSize(dataCounter)}|${bytesToSize(planMonthlyData)}`,
+        `重置：${dataNextReset.getFullYear()}年${dataNextReset.getMonth() + 1}月${dataNextReset.getDate()}日`
+    ];
 
     let now = new Date();
     let hour = now.getHours();
@@ -15,8 +38,8 @@ $httpClient.get(requestUrl, function(error, response, data){
     minutes = minutes > 9 ? minutes : "0" + minutes;
 
     $done({
-        title: `Bandwagon VPS Info | ${hour}:${minutes}`,
-        content: `Node IP: ${nodeIp}\nOS: ${os}`,
+        title: `Bandwagon Info | ${hour}:${minutes}`,
+        content: content.join("\n"),
         icon: "server.rack",
         "icon-color": "#3498db",
     });
@@ -28,4 +51,11 @@ function getArgs() {
             .split("&")
             .map((item) => item.split("="))
     );
+}
+
+function bytesToSize(bytes) {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes == 0) return '0 Byte';
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
 }
