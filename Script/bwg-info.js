ff4dcc55ec85f6c003cdbf5f1997d8aca371ae41
libli <libli@tencent.com>
2023-10-06 14:41:45 +0800

    add module

--- Script/bwg-info.js
+++ Script/bwg-info.js
@@ -1,8 +1,24 @@
-$httpClient.get("https://api.64clouds.com/v1/getServiceInfo?veid=1826936&api_key=" + $argument, function(error, response, data){
+let args = getArgs();
+let requestUrl = `https://api.64clouds.com/v1/getServiceInfo?veid=${args.veid}&api_key=${args.api_key}`;
+
+$httpClient.get(requestUrl, function(error, response, data){
+    // 解析 JSON 数据
+    let jsonData = JSON.parse(data);
+
+    let nodeIp = jsonData.node_ip;
+    let os = jsonData.os;
     $done({
         title: `Bandwagon VPS Info | ${hour}:${minutes}`,
-        content: data,
+        content: `Node IP: ${nodeIp}\nOS: ${os}`,
         icon: "server.rack",
         "icon-color": "#3498db",
     });
 });
+
+function getArgs() {
+    return Object.fromEntries(
+        $argument
+            .split("&")
+            .map((item) => item.split("="))
+    );
+}
