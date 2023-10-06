3b3ada27a0a1166d9424f0953c65fa5dfd4af89c
libli <libli@tencent.com>
2023-10-06 14:46:17 +0800

    add module

--- Script/bwg-info.js
+++ Script/bwg-info.js
@@ -7,6 +7,13 @@ $httpClient.get(requestUrl, function(error, response, data){
 
     let nodeIp = jsonData.node_ip;
     let os = jsonData.os;
+
+    let now = new Date();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    hour = hour > 9 ? hour : "0" + hour;
+    minutes = minutes > 9 ? minutes : "0" + minutes;
+
     $done({
         title: `Bandwagon VPS Info | ${hour}:${minutes}`,
         content: `Node IP: ${nodeIp}\nOS: ${os}`,
